c30010434899ba01835bb423a50f9325bf557eaa
Vedantlearn <112773665+Vedantlearn@users.noreply.github.com>
2024-08-03 20:38:27 +0530

    Update Calculator.java

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -12,7 +12,7 @@ public class Calculator implements ActionListener {
       
 	Font Myfont = new Font("ink free", Font.BOLD, 30); 
 
-	double num1 = 0, num2 = 0, result = 0;                
+	double num1 = 0, num2 = 0, result = 0;                                                                
 	char operator;
 
 	Calculator() {
